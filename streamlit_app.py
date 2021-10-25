eb0e14ebe5de78e2af9d7e26023f457f21531b10
gonced8 <gonced8@gmail.com>
2021-10-25 22:41:15 +0100

    test e-mail

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,39 @@
 import streamlit as st
 
 st.title("Gerador de Recibos")
+
+if st.button("Send e-mail"):
+    smtp_server = st.secrets["EMAIL"]["server"]
+    port = int(st.secrets["EMAIL"]["port"])  # For starttls
+    sender_email = st.secrets["EMAIL"]["email"]
+    password = st.secrets["EMAIL"]["password"]
+    sender = st.secrets["EMAIL"]["sender"]
+
+    msg = EmailMessage()
+
+    # Header
+    msg["Subject"] = "Recibos mensalidade SCBL"
+    msg["From"] = f"{sender} <{sender_email}>"
+    msg["To"] = "Gonçalo Raposo <gonced8@gmail.com>"
+
+    # Plain text Body
+    message = (
+        "Segue em anexo os recibos:\n"
+        f"TESTE\n"
+        "\n"
+        "SCBL\n"
+        "\n"
+        "(Este e-mail foi gerado automaticamente.)"
+    )
+    msg.set_content(message)
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    # Send e-mail
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        # server.set_debuglevel(1)
+        server.send_message(msg)
+
+    print(f"Sent receipt {' '.join(filenames)} to {name} at {receiver_email}.")
